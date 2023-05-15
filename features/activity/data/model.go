7db8f694c16fa1d5dc39ef65997b7b137bdc59d4
alfianadityads <alfianadityadwi@gmail.com>
2023-05-15 21:52:53 +0700

    feat: make activity function model to core and core to model

--- features/activity/data/model.go
+++ features/activity/data/model.go
@@ -1,10 +1,36 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"todolistapi/features/activity"
+
+	"gorm.io/gorm"
+)
 
 type Activity struct {
 	gorm.Model
 	Title string
 	Email string
 	// Todo []data.Todo `gorm:"foreignkey:ActivityGroupID"`
+}
+
+func CoreToModel(data activity.Core) Activity {
+	return Activity{
+		Model: gorm.Model{
+			ID:        data.ID,
+			CreatedAt: data.CreateAt,
+			UpdatedAt: data.UpdateAt,
+		},
+		Title: data.Title,
+		Email: data.Email,
+	}
+}
+
+func ModelToCore(data Activity) activity.Core {
+	return activity.Core{
+		ID:       data.ID,
+		Title:    data.Title,
+		Email:    data.Email,
+		CreateAt: data.CreatedAt,
+		UpdateAt: data.UpdatedAt,
+	}
 }
