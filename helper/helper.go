1d2dabe284192d3e098b1de81c8f4e64b844ff61
alfianadityads <alfianadityadwi@gmail.com>
2023-05-15 20:32:53 +0700

    chore: add function for validation

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,13 @@
 package helper
 
+import (
+	"errors"
+	"log"
+	"strings"
+
+	"github.com/go-playground/validator"
+)
+
 type ResponseWithData struct {
 	Status  string      `json:"status"`
 	Message string      `json:"message"`
@@ -27,4 +35,28 @@ func APIResponseWitouthData(status, message string) ResponseWithoutData {
 	}
 
 	return res
+}
+
+var validate *validator.Validate
+
+func Validation(data interface{}) error {
+	validate = validator.New()
+	err := validate.Struct(data)
+	if err != nil {
+		log.Println(err)
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			log.Println(err)
+		}
+		msg := ""
+		if strings.Contains(err.Error(), "required") {
+			msg = "Body request cannot be blank"
+		} else if strings.Contains(err.Error(), "title") {
+			msg = "title cannot be null"
+		} else if strings.Contains(err.Error(), "email") {
+			msg = "email cannot be null"
+		}
+		return errors.New(msg)
+	}
+	
+	return nil
 }
