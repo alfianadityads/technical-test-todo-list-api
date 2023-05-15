a94ef6dae54ad97d596e008ddb2f55dce8887fc1
alfianadityads <alfianadityadwi@gmail.com>
2023-05-15 20:28:08 +0700

    chore: make function helper for response with or without data

--- helper/helper.go
+++ helper/helper.go
@@ -10,3 +10,21 @@ type ResponseWithoutData struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
 }
+
+func APIResponseWithData(status string, message string, data interface{}) ResponseWithData {
+	res := ResponseWithData{
+		Message: message,
+		Status:  status,
+		Data:    data,
+	}
+	return res
+}
+
+func APIResponseWitouthData(status, message string) ResponseWithoutData {
+	res := ResponseWithoutData{
+		Message: message,
+		Status:  status,
+	}
+
+	return res
+}
