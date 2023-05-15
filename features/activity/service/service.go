e62c7b7a5b93191109fec5c6a307b3512aad3d42
alfianadityads <alfianadityadwi@gmail.com>
2023-05-15 23:36:00 +0700

    feat: make create service function

---
+++ features/activity/service/service.go
@@ -0,0 +1,55 @@
+package service
+
+import (
+	"todolistapi/features/activity"
+	"todolistapi/helper"
+
+	"github.com/go-playground/validator"
+)
+
+type activityService struct {
+	qry activity.ActivityData
+	vld *validator.Validate
+}
+
+func New(ad activity.ActivityData) activity.ActivityService {
+	return &activityService{
+		qry: ad,
+		vld: validator.New(),
+	}
+}
+
+// Create implements activity.ActivityService
+func (as *activityService) Create(newActivity activity.Core) (activity.Core, error) {
+	err := helper.Validation(newActivity)
+	if err != nil {
+		return newActivity, err
+	}
+
+	res, err := as.qry.Create(newActivity)
+	if err != nil {
+		return res, err
+	}
+
+	return res, nil
+}
+
+// Delete implements activity.ActivityService
+func (as *activityService) Delete(activityID uint) error {
+	panic("unimplemented")
+}
+
+// GetAll implements activity.ActivityService
+func (as *activityService) GetAll() ([]activity.Core, error) {
+	panic("unimplemented")
+}
+
+// GetOne implements activity.ActivityService
+func (as *activityService) GetOne(activityID uint) (activity.Core, error) {
+	panic("unimplemented")
+}
+
+// Update implements activity.ActivityService
+func (as *activityService) Update(activityID uint, updateActivity activity.Core) (activity.Core, error) {
+	panic("unimplemented")
+}
