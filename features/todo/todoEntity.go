2cfa87da4ba7904365b34f64927785d983b8419c
alfianadityads <alfianadityadwi@gmail.com>
2023-05-16 21:56:36 +0700

    feat: make todo handler, service, and data interface

--- features/todo/todoEntity.go
+++ features/todo/todoEntity.go
@@ -1,6 +1,10 @@
 package todo
 
-import "time"
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
 
 type Core struct {
 	ID              uint
@@ -10,4 +14,27 @@ type Core struct {
 	ActivityGroupID uint
 	CreatedAt       time.Time
 	UpdatedAt       time.Time
+}
+
+type TodoHandler interface {
+	Create() echo.HandlerFunc
+	GetOne() echo.HandlerFunc
+	GetAll() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+}
+type TodoService interface {
+	Create(newTodo Core) (Core, error)
+	GetOne(id uint) (Core, error)
+	GetAll(actID uint) ([]Core, error)
+	Update(id uint, updatedTodo Core) (Core, error)
+	Delete(id uint) error
+}
+
+type TodoData interface {
+	Create(newTodo Core) (Core, error)
+	GetOne(id uint) (Core, error)
+	GetAll(actID uint) ([]Core, error)
+	Update(id uint, updatedTodo Core) (Core, error)
+	Delete(id uint) error
 }
