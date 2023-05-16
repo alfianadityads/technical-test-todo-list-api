312818983c9aaf476734781b07148c60c2ccc883
alfianadityads <alfianadityadwi@gmail.com>
2023-05-17 00:25:06 +0700

    chore: route endpoint for todo feature

--- app/route/route.go
+++ app/route/route.go
@@ -7,12 +7,20 @@ import (
 	activData "todolistapi/features/activity/data"
 	activHandler "todolistapi/features/activity/handler"
 	activService "todolistapi/features/activity/service"
+
+	todoData "todolistapi/features/todo/data"
+	todoHdl "todolistapi/features/todo/handler"
+	todoSrv "todolistapi/features/todo/service"
 )
 
 func InitRoute(db *gorm.DB, e *echo.Echo) {
 	activityData := activData.New(db)
 	activityService := activService.New(activityData)
 	activityHandler := activHandler.New(activityService)
+
+	todoData := todoData.New(db)
+	todoService := todoSrv.New(todoData)
+	todoHandler := todoHdl.New(todoService)
 
 	// Activity
 	act := e.Group("/activity-groups")
@@ -22,4 +30,13 @@ func InitRoute(db *gorm.DB, e *echo.Echo) {
 	act.GET("/:id", activityHandler.GetOne())
 	act.PATCH("/:id", activityHandler.Update())
 	act.DELETE("/:id", activityHandler.Delete())
+
+	// Todo
+	todo := e.Group("/todo-items")
+
+	todo.POST("", todoHandler.Create())
+	todo.GET("/:id", todoHandler.GetOne())
+	todo.GET("", todoHandler.GetAll())
+	todo.PATCH("/:id", todoHandler.Update())
+	todo.DELETE("/:id", todoHandler.Delete())
 }
